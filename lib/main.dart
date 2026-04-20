1d9ba481c44942cbf311fa0492a66fe43b31a6d7
humayunkabir11 <humayan180024@diit.edu.bd>
2026-04-20 23:12:56 +0600

    main

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'dart:async';
+
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'core/config/routes/app_route.dart';
+import 'core/config/strings/app_strings.dart';
+import 'core/di/init_dependencies.dart';
+import 'core/utils/dev_logs.dart';
+import 'features/news/presentation/bloc/news_bloc.dart';
+
+
+void main() async {
+  await runZonedGuarded<Future<void>>(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await Hive.initFlutter();
+      await initDependencies(); // ✅ VERY IMPORTANT
+
+      /// ------------- device orientation/ (off rotation)--------
+      await SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown,
+      ]);
+
+      /// --------------------- status bar -------------
+      SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.black,
+        statusBarIconBrightness: Brightness.dark,
+        systemNavigationBarIconBrightness: Brightness.dark,
+      ));
+
+      runApp(
+        DevicePreview(
+          enabled: false,
+          tools: const [...DevicePreview.defaultTools],
+          builder: (context) => const MyApp(),
+        ),
+      );
+    },
+        (error, stackTrace) {
+      devLog(
+        tag: "APPLICATION-ERROR",
+        payload: {"error": "$error", "stackTrace": "$stackTrace"},
+      );
+    },
+  );
+
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: false,
+      useInheritedMediaQuery: true,
+      builder: (context, child) {
+        return MultiBlocProvider(
+          providers: [
+            BlocProvider.value(value: sl<NewsBloc>()),
+          ],
+          child: MediaQuery(
+            data: MediaQuery.of(
+              context,
+            ).copyWith(textScaler: const TextScaler.linear(1.0)),
+            child: MaterialApp.router(
+              builder: DevicePreview.appBuilder,
+              debugShowCheckedModeBanner: false,
+              title: AppStrings.appName,
+              routeInformationParser: AppRoute.router.routeInformationParser,
+              routerDelegate: AppRoute.router.routerDelegate,
+              routeInformationProvider:
+              AppRoute.router.routeInformationProvider,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
